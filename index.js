c7a77527c0a8d37a2351ccf5919ce23fbaa87f5e
Henrique Vazquez <henrique_pm_vazquez@hotmail.com>
2023-03-21 08:14:48 -0300

    Consumindo com axios

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 
@@ -7,9 +8,11 @@ app.listen('3000');
 //Middleware
 app.use(express.json());
 
-app.route('/').get((req, res) => res.send(req.query.name));
-
-app.route('/about/user').get((req, res) => res.send(req.query));
+app.route('/').get((req, res) => {
+  axios.get('https://api.github.com/users/henriquevazquez')
+    .then(result => res.send(`<img src="${result.data.avatar_url}"/>`))
+    .catch((error) => console.error(error));
+});
 
 
 
